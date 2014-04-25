50384edb53c90fd7653f9520325b3bf32a742ec2
gonzaloGarcia <gonzalogarcia93@gmail.com>
2014-04-25 11:44:55 -0300

    Agrego el test de Tabla de conversión.

--- DDS-correctorParciales/src/correctorParciales/TablaConversion.java
+++ DDS-correctorParciales/src/correctorParciales/TablaConversion.java
@@ -1,28 +1,33 @@
 package correctorParciales;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Stream;
 
 public class TablaConversion implements MetodoCorreccion {
-	
-	private Collection<ParDeConversion> paresConversion;
-	
-	public Collection<ParDeConversion> getParesConversion(){
+
+	private ArrayList<ParDeConversion> paresConversion = new ArrayList<ParDeConversion>();
+
+	public Collection<ParDeConversion> getParesConversion() {
 		return this.paresConversion;
 	}
-	
+
 	public Stream<ParDeConversion> getStreamDePares() {
 		return this.getParesConversion().stream();
 	}
-	
-	public void setNuevoParConversion(double unPuntaje, double unaNotaCorrespondiente) {
-		ParDeConversion unPar = new ParDeConversion(unPuntaje, unaNotaCorrespondiente);
+
+	public void setNuevoParConversion(double unPuntaje,
+			double unaNotaCorrespondiente) {
+		ParDeConversion unPar = new ParDeConversion(unPuntaje,
+				unaNotaCorrespondiente);
 		this.getParesConversion().add(unPar);
-				
+
 	}
 
-	public double notaParaExamen(Examen unExamen){
-		return this.getStreamDePares().filter(par-> par.coincideCon(unExamen.puntajeTotal())).findAny().get().getNotaCorrespondiente();
-		}
-	
+	public double notaParaExamen(Examen unExamen) {
+		return this.getStreamDePares()
+				.filter(par -> par.coincideCon(unExamen.puntajeTotal()))
+				.findAny().get().getNotaCorrespondiente();
+	}
+
 }
---
+++ DDS-correctorParciales/test/correctorParciales/TablaConversionTest.java
@@ -0,0 +1,42 @@
+package correctorParciales;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class TablaConversionTest {
+
+	@Test
+	public void test() {
+		Pregunta unaDificil = new Pregunta();
+		unaDificil.setPesoEspecifico(5);
+		unaDificil.setRespuestaCorrecta("UML");
+		Pregunta unaFacil = new Pregunta();
+		unaFacil.setPesoEspecifico(1);
+		unaFacil.setRespuestaCorrecta("Ese código carece de cohesión.");
+		
+		RespuestaDada primerRespuesta = new RespuestaDada();
+		primerRespuesta.setvalor("UML");
+		primerRespuesta.setPreguntaQueContesta(unaDificil);
+		RespuestaDada segundaRespuesta = new RespuestaDada();
+		segundaRespuesta.setvalor("Me huele mal.");
+		segundaRespuesta.setPreguntaQueContesta(unaFacil);
+		ArrayList<RespuestaDada> respuestas= new ArrayList<RespuestaDada>();
+		respuestas.add(primerRespuesta);
+		respuestas.add(segundaRespuesta);
+		
+		Examen examen = new Examen();
+
+		TablaConversion tablita = new TablaConversion();
+		
+		examen.setMetodoCorreccion(tablita);
+		examen.setRespuestasDadas(respuestas);
+		
+		tablita.setNuevoParConversion(10,5);
+		
+		assertTrue(tablita.notaParaExamen(examen)==5);
+		
+	}
+}
